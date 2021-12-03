d68ed59da5438f3b622e54c4aff9f862be84d6e1
Kshitij Kakade <kshitijskakade307@gmail.com>
2021-12-03 19:53:55 +0530

    Finished with Header.js and Home.js Components Styling and Functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login.js";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
+          <Route
+            exact
+            path="/home"
+            element={
+              <div>
+                <Header />
+                <Home />
+              </div>
+            }
+          ></Route>
         </Routes>
       </Router>
     </div>
